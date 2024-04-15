64345502cfd256d4c8ba170f134d5c698264ec53
KMBedw <edwin3@live.fr>
2024-04-15 16:04:45 +0200

    test unitaire

--- __test__/calculate-age.test.js
+++ __test__/calculate-age.test.js
@@ -3,12 +3,12 @@ const user = require('../src/data/user');
 const calculate_age = require('../src/helpers/calculate-age');
 
 describe('unit test suite', () => {
-  it('should throw error', () => {
-    try {
-        calculate_age("2022-01-02");
-    } catch (error) {
-        expect(error).toBeInstanceOf(Error)
-        expect(error.message).toContain('Cannot read properties of undefined');
-    }
+  it('should throw an error for invalid input', () => {
+    const invalidInput = "2022-01-02";
+    
+    expect(() => {
+      calculate_age({ birth: invalidInput });
+    }).toThrow("Invalid input: 'birth' should be a Date instance.");
   });
 });
+
--- src/helpers/calculate-age.js
+++ src/helpers/calculate-age.js
@@ -1,7 +1,10 @@
 const calculate_age = (user) => {
+  if (!user || !user.birth || !(user.birth instanceof Date)) {
+    throw new Error("Invalid input: 'birth' should be a Date instance.");
+  }
   let date_diff = new Date(Date.now() - user.birth.getTime()),
-    age = Math.abs(date_diff.getUTCFullYear() - 1970);
+      age = Math.abs(date_diff.getUTCFullYear() - 1970);
   return age;
 }
 
-module.exports = calculate_age
+module.exports = calculate_age;
