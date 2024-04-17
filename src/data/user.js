906f9e6f972879a378b0f2116395707191a73995
KMBedw <edwin3@live.fr>
2024-04-17 10:14:42 +0200

    test unitaire orchestration 17avril_n3

--- src/data/user.js
+++ src/data/user.js
@@ -3,5 +3,4 @@ const user = {
   // Assurez-vous que la propriété 'birth' est une instance de Date
   birth: new Date('1992-05-09'), // Cette ligne a été rétablie à une Date
 };
-
 module.exports = user;
